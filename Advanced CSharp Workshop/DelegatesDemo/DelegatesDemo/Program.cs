e529111627bc130f97d701f7333075a4e05cd1a7
Jonne <jonnekats@gmail.com>
2013-02-26 20:27:59 +0100

    First version demo

---
+++ Advanced CSharp Workshop/DelegatesDemo/DelegatesDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    class Program
+    {
+        public delegate void Say(string subject);
+
+        public static void Hello(string subject)
+        {
+            Console.WriteLine("Hello " + subject);
+        }
+
+        public static void Goodbye(string subject)
+        {
+            Console.WriteLine("Goodbye " + subject);
+        }
+
+        private static void Main(string[] args)
+        {
+            Say hello = Hello;
+            Say goodbye = Goodbye;
+
+            Say both = (Say)Delegate.Combine(hello, goodbye);
+
+            Say bothInDifferentWay = hello + goodbye;
+
+            Console.WriteLine("Invoking hello");
+            hello("test");
+
+            Console.WriteLine("Invoking goodbye");
+            goodbye("test");
+
+            Console.WriteLine("Invoking both");
+            both("test");
+
+            Console.WriteLine("Invoking bothInDifferentWay");
+            bothInDifferentWay("test");
+
+            Console.ReadKey();
+        }
+    }
+}
