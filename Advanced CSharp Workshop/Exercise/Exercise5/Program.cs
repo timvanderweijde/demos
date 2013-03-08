53e3df8f37f831a65eb0c1bc6dda3fd57a5fbec8
Jonne <jonnekats@gmail.com>
2013-03-08 14:54:54 +0100

    Added exercise for expression trees

---
+++ Advanced CSharp Workshop/Exercise/Exercise5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var person = new Person {Name = "bill"};
+
+            LogPropertyNameAndValue(() => person.Name);
+
+            Console.ReadKey();
+        }
+
+        private static void LogPropertyNameAndValue<T>(Func<T> accessProperty)
+        {
+            Console.WriteLine("Property value:{0}", accessProperty());
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
