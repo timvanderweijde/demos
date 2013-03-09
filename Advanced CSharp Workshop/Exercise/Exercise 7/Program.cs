12adf7bb045796a840f3db03cf5bb22761e41923
Jonne <jonnekats@gmail.com>
2013-03-09 14:42:18 +0100

    Added PLinq exercise

---
+++ Advanced CSharp Workshop/Exercise/Exercise 7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace Exercise_7
+{
+    public class Order
+    {
+        public string CustomerName { get; set; }
+        public decimal OrderTotal { get; set; }
+
+        public decimal CalculateTotalOfOrders()
+        {
+            // Lets pretend okay?
+            Thread.Sleep(5);
+
+            return OrderTotal;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var random = new Random();
+
+            IEnumerable<Order> orders = Enumerable.Range(1, 1000).Select(i => new Order
+                {
+                    CustomerName = "Customer" + i,
+                    OrderTotal = random.Next(0, 1000)
+                });
+
+            Stopwatch sw = Stopwatch.StartNew();
+            Console.WriteLine("Calculation started");
+
+            var totals = orders.Select(o => new {o.CustomerName, OrderTotal = o.CalculateTotalOfOrders()});
+
+            foreach (var total in totals)
+            {
+                InvokeSomeWebService(total.CustomerName, total.OrderTotal);
+            }
+
+            long elapsed = sw.ElapsedMilliseconds;
+
+            Console.Write("Finished in {0} ms", elapsed);
+            Console.ReadKey();
+        }
+
+        private static void InvokeSomeWebService(string customerName, decimal orderTotal)
+        {
+            Thread.Sleep(5);
+        }
+    }
+}
