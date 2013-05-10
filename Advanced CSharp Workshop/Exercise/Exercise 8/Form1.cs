ae338a62c587effd7623e47ccdb39279e2562866
Jonne <jonnekats@gmail.com>
2013-05-10 14:05:44 +0200

    Added solution for Excercise 8

--- Advanced CSharp Workshop/Exercise/Exercise 8/Form1.cs
+++ Advanced CSharp Workshop/Exercise/Exercise 8/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Exercise_8.Service;
 
@@ -8,26 +9,31 @@ namespace Exercise_8
 {
     public partial class Form1 : Form
     {
+        private IEnumerable<Division> divisions;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private IEnumerable<Division> divisions;
-
         private void Form1_Load(object sender, EventArgs e)
         {
             var financialService = new FinancialService();
 
-            // First get the divisions, because the region combo depends on this
-            divisions = financialService.GetDivisions();
+            Task.Factory.StartNew(() =>
+                {
+                    // First get the divisions, because the region combo depends on this
+                    divisions = financialService.GetDivisions();
 
-            regionCombo.DataSource =  financialService.GetRegions();
+                    return financialService.GetRegions();
+                })
+                .ContinueWith(x => { regionCombo.DataSource = x.Result; },
+                              TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private void regionCombo_SelectedValueChanged(object sender, EventArgs e)
         {
-            divisionCombo.DataSource = divisions.Where(d => d.Region == (Region)regionCombo.SelectedValue).ToList();
+            divisionCombo.DataSource = divisions.Where(d => d.Region == (Region) regionCombo.SelectedValue).ToList();
         }
     }
 }
