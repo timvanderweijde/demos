940780f84205fd0effba4bd6124204b75a4bc0de
Jonne <jonnekats@gmail.com>
2013-05-10 15:03:29 +0200

    Added exception handling

--- Advanced CSharp Workshop/Exercise/Exercise 8/Form1.cs
+++ Advanced CSharp Workshop/Exercise/Exercise 8/Form1.cs
@@ -9,36 +9,46 @@ namespace Exercise_8
 {
     public partial class Form1 : Form
     {
+        private readonly FinancialService financialService = new FinancialService();
+        private IEnumerable<Division> divisions;
+
         public Form1()
         {
             InitializeComponent();
         }
-
-        private IEnumerable<Division> divisions;
-        FinancialService financialService = new FinancialService();
 
         private void Form1_Load(object sender, EventArgs e)
         {
             // First get the divisions, because the region combo depends on this
             divisions = financialService.GetDivisions();
 
-            regionCombo.DataSource =  financialService.GetRegions();
+            regionCombo.DataSource = financialService.GetRegions();
         }
 
         private void regionCombo_SelectedValueChanged(object sender, EventArgs e)
         {
-            divisionCombo.DataSource = divisions.Where(d => d.Region == (Region)regionCombo.SelectedValue).ToList();
+            divisionCombo.DataSource = divisions.Where(d => d.Region == (Region) regionCombo.SelectedValue).ToList();
         }
 
         private void calculateRevenueButton_Click(object sender, EventArgs e)
         {
             cancelButton.Enabled = true;
+            revenueTextBox.Text = "Calculating";
 
-            decimal revenue = financialService.GetRevenue((Region) regionCombo.SelectedValue);
+            try
+            {
+                decimal revenue = financialService.GetRevenue((Region) regionCombo.SelectedValue);
 
-            revenueTextBox.Text = revenue.ToString(CultureInfo.InvariantCulture);
-
-            cancelButton.Enabled = false;
+                revenueTextBox.Text = revenue.ToString(CultureInfo.InvariantCulture);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cancelButton.Enabled = false;
+            }
         }
     }
 }
